ef1071e503921d4758f2633dd063da65d450b444
Oxrainanco12 <102026325+Oxrainanco12@users.noreply.github.com>
2023-11-20 21:26:11 +0700

    Add files via upload

---
+++ Lab7/src/Lab/GA_NQueenAlgo.java
@@ -0,0 +1,81 @@
+package Lab;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class GA_NQueenAlgo {
+	public static final int POP_SIZE = 100;// Population size
+	public static final double MUTATION_RATE = 0.03;
+	public static final int MAX_ITERATIONS = 1000;
+	List<Node> population = new ArrayList<Node>();
+	Random rd = new Random();
+
+	// initialize the individuals of the population
+	public void initPopulation() {
+		for (int i = 0; i < POP_SIZE; i++) {
+			Node ni = new Node();
+			ni.generateBoard();
+			population.add(ni);
+		}
+	}
+
+	public Node execute() {
+		initPopulation();
+		int k = 0;
+		while (k++ < MAX_ITERATIONS) {
+			ArrayList<Node> newPopulation = new ArrayList<>();
+			for (int i = 0; i < POP_SIZE; i++) {
+				Node x = getParentByRandomSelection();
+				Node y = getParentByRandomSelection();
+				Node child = reproduce(x, y);
+				if (rd.nextDouble() < MUTATION_RATE) {
+					mutate(child);
+
+				}
+				if (child.getH() == 0)
+					return child;
+				population.add(child);
+			}
+			population = newPopulation;
+		}
+		Collections.sort(population);
+		return population.get(0);
+
+	}
+
+	// Mutate an individual by selecting a random Queen and
+	// move it to a random row.
+	public void mutate(Node node) {
+		// Enter your code here
+		// return null;
+	}
+
+	// Crossover x and y to reproduce a child
+	public Node reproduce(Node x, Node y) {
+		Node child = new Node();
+		child.generateBoard();
+		int c = rd.nextInt(Node.N);
+		for (int i = 0; i < c; i++) {
+			child.setRow(i, x.getRow(i));
+			for (int j = c; j < Node.N; j++) {
+				child.setRow(i, y.getRow(i));
+			}
+		}
+		return child;
+	}
+
+	// Select K individuals from the population at random and
+	// select the best out of these to become a parent.
+	public Node getParentByTournamentSelection() {
+		// Enter your code here
+		return null;
+	}
+
+	// Select a random parent from the population
+	public Node getParentByRandomSelection() {
+		// Enter your code here
+		return null;
+	}
+}
